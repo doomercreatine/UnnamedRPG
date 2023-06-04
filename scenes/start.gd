3cd0827c43700dd5ba8031b5043cf59e4949d96f
DoomerCreatine <doomercreatine@outlook.com>
2023-06-04 09:57:04 -0700

    Move global controls to control node
    Moved the fullscreen and quit commands into the main Control node

--- scenes/start.gd
+++ scenes/start.gd
@@ -1,2 +1,14 @@
 extends Control
 
+	
+func _unhandled_input(event) -> void:
+	if event.is_action_pressed("toggle_fullscreen"):
+		swap_fullscreen_mode()
+	if event.is_action_pressed("ui_cancel"):
+		get_tree().quit()
+
+func swap_fullscreen_mode() -> void:
+	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
--- scripts/world.gd
+++ scripts/world.gd
@@ -6,18 +6,6 @@ func _ready() -> void:
 	$TileMap/Cursor.show()
 	player.find_child("VisionLight").visible = true
 	
-	
-func _unhandled_input(event) -> void:
-	if event.is_action_pressed("toggle_fullscreen"):
-		swap_fullscreen_mode()
-	if event.is_action_pressed("ui_cancel"):
-		get_tree().quit()
-
-func swap_fullscreen_mode() -> void:
-	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
-	else:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
 
 
 
